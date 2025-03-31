f6ec5a9fe6a0c1c80616cd09366ab948387a610b
villageworks <villageworks@gmail.com>
2025-04-01 01:51:04 +0900

    ver.0.20: Implementation of category-based filtering search functionality

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Category;
 import com.example.demo.entity.Item;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.ItemRepository;
 
 @Controller
@@ -16,16 +19,33 @@ public class ItemController {
 	@Autowired
 	ItemRepository itemRepository;
 	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	/**
 	 * 商品一覧画面表示
 	 * @return 商品一覧画面のThymeleafテンプレート名
 	 */
 	@GetMapping("/items")
-	public String index(Model model) {
-		// 商品一覧の商品リストを取得
-		List<Item> itemList = itemRepository.findAll();
-		// 取得した商品リストをスコープに登録
+	public String index(@RequestParam(required = false) Integer categoryId,
+						Model model) {
+		// カテゴリリストを取得
+		List<Category> categoryList = categoryRepository.findAll();
+		
+		// カテゴリIDによって処理を分岐
+		List<Item> itemList = null;
+		if (categoryId == null) {
+			// 全商品検索
+			itemList = itemRepository.findAll();
+		} else {
+			// カテゴリ検索
+			itemList = itemRepository.findByCategoryId(categoryId);
+		}
+		
+		// 取得したカテゴリリストと商品リストをスコープに登録
+		model.addAttribute("categories", categoryList);
 		model.addAttribute("items", itemList);
+		
 		// 画面遷移
 		return "items";
 	}
--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+	/**
+	 * カテゴリ検索：SELECT * FROM items WHERE categoryId = ?
+	 * @param categoryId カテゴリID
+	 * @return 商品リスト
+	 */
+	List<Item> findByCategoryId(Integer categoryId);
 
 }
